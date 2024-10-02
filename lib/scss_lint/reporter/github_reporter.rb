ac3b64bf2b3b7abdb4a4ed43eba97fd4d330493d
Nick Bucciarelli <nick.bucciarelli@wantable.com>
2024-10-02 13:38:45 -0400

    Github reporter

--- lib/scss_lint/reporter/github_reporter.rb
+++ lib/scss_lint/reporter/github_reporter.rb
@@ -6,21 +6,21 @@ module SCSSLint
 
       # "#{lints.map { |lint| "#{location(lint)} #{type(lint)} #{message(lint)}" }.join("\n")}\n"
       # ::#{severity} file=#{lint.filename},line=#{lint.line}::#{github_escape(lint.message)}"
-      "#{lints.map { |lint| "::#{type(lint)} file=#{location(lint)},line=#{123}::#{message(lint)}" }.join("\n")}\n"
+      "#{lints.map { |lint| "::#{type(lint)} #{location(lint)}::#{message(lint)}" }.join("\n")}\n"
     end
 
   private
 
     def location(lint)
       [
-        log.cyan(lint.filename),
-        log.magenta(lint.location.line.to_s),
-        log.magenta(lint.location.column.to_s),
-      ].join(':')
+        "file=#{log.cyan(lint.filename)}",
+        "line=#{log.magenta(lint.location.line.to_s)}"
+        # log.magenta(lint.location.column.to_s),
+      ].join(',')
     end
 
     def type(lint)
-      lint.error? ? log.red('::error') : log.yellow('::warning')
+      lint.error? ? log.red('error') : log.yellow('warning')
     end
 
     def message(lint)

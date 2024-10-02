813f3e30269036dd58babbad426254e83e805e53
Nick Bucciarelli <nick.bucciarelli@wantable.com>
2024-10-02 14:48:40 -0400

    Spec

--- lib/scss_lint/reporter/github_reporter.rb
+++ lib/scss_lint/reporter/github_reporter.rb
@@ -1,31 +1,22 @@
 module SCSSLint
-  # Reports a single line per lint.
   class Reporter::GithubReporter < Reporter
     def report_lints
       return unless lints.any?
 
-      # "#{lints.map { |lint| "#{location(lint)} #{type(lint)} #{message(lint)}" }.join("\n")}\n"
-      # ::#{severity} file=#{lint.filename},line=#{lint.line}::#{github_escape(lint.message)}"
-      "#{lints.map { |lint| "::error #{location(lint)}::#{message(lint)}" }.join("\n")}\n"
+      "#{lints.map { |lint| "::error #{location(lint)}::#{message(lint)}" }.join("\n")}"
     end
 
   private
 
     def location(lint)
       [
-        "file=#{log.cyan(lint.filename)}",
-        "line=#{log.magenta(lint.location.line.to_s)}"
-        # log.magenta(lint.location.column.to_s),
+        "file=#{lint.filename}",
+        "line=#{lint.location.line.to_s}"
       ].join(',')
     end
 
-    def type(lint)
-      lint.error? ? log.red('error') : log.yellow('warning')
-    end
-
     def message(lint)
-      linter_name = log.green("#{lint.linter.name}: ")
-      "#{linter_name}#{lint.description}"
+      "#{lint.linter.name}:#{lint.description}"
     end
   end
 end
